bc17dd5dda28954f64696b119f6d27ea379c31e8
Thomas Park <thomas@thomaspark.me>
2015-03-09 23:55:15 -0400

    complete metrics

--- metrics/script.js
+++ metrics/script.js
@@ -20,7 +20,13 @@
   function analyze(data) {
     stats.elements = {};
     stats.count = 0;
-    stats.unique = 0;
+    stats.types = 0;
+    stats.internal = 0;
+    stats.depth = 0;
+    stats.max = 0;
+    stats.indents = 0;
+    stats.entropy = 0;
+    stats.halstead = 0;
 
     if (strict) {
       try {
@@ -29,7 +35,7 @@
         });
       } catch(err) {
         console.log(err);
-        $("#metrics").html("<p>Invalid XML</p>");
+        $("#metrics").html("<h2><i class='fa fa-exclamation-triangle'></i> Invalid XML</h2>");
         return;
       }
     } else {
@@ -40,26 +46,23 @@
 
     for (var e in stats.elements) {
       stats.count += stats.elements[e];
-      stats.unique += 1;
-      // console.log(stats.elements[e], e);
+      stats.types += 1;
     }
 
     loc(data);
+    indents(data);
+    halstead();
 
     var metrics = $("#metrics");
     metrics.empty();
     metrics.append("<p>Lines of Code<br>" + stats.length + "</p>");
-    metrics.append("<p>Element Count<br>" + stats.count + "</p>");
-    metrics.append("<p>Unique Element Count<br>" + stats.unique + "</p>");
-    metrics.append("<p>McCabe's Cyclomatic Complexity<br>" + "0" + "</p>");
-    metrics.append("<p>Halstead Metric<br>" + "0" + "</p>");
-    metrics.append("<p>Average Breadth<br>" + "0" + "</p>");
-    metrics.append("<p>Average Depth<br>" + "0" + "</p>");
-    metrics.append("<p>Internal Nodes<br>" + "0" + "</p>");
-    metrics.append("<p>Nesting Metric<br>" + "0" + "</p>");
-    metrics.append("<p>Entropy<br>" + "0" + "</p>");
-
-
+    metrics.append("<p>Elements<br>" + stats.count + "</p>");
+    metrics.append("<p>Element Types<br>" + stats.types + "</p>");
+    metrics.append("<p>Internal Elements<br>" + stats.internal + "</p>");
+    metrics.append("<p>Max Depth<br>" + stats.max + "</p>");
+    metrics.append("<p>Total Depth<br>" + stats.depth + "</p>");
+    metrics.append("<p>Total Indents<br>" + stats.indents + "</p>");
+    metrics.append("<p>Halstead Volume<br>" + stats.halstead.toFixed(1) + "</p>");
   }
 
   function traverse(elem, level) {
@@ -68,7 +71,15 @@
       return;
     }
 
-    console.log(elem.tagName, level);
+    stats.depth += level;
+
+    if ($(elem).children().length > 0) {
+      stats.internal += 1;
+    }
+
+    if (level > stats.max) {
+      stats.max = level;
+    }
 
     if (stats.elements[elem.tagName]) {
       stats.elements[elem.tagName] += 1;
@@ -83,6 +94,20 @@
 
   function loc(data) {
     stats.length = data.split(/\r*\n/).length;
+  }
+
+  function indents(data) {
+    var lines = data.split("\n");
+    lines.forEach(function(line) {
+      var index = 0;
+      while (line.charAt(index++) === "\t") {
+        stats.indents++;
+      }
+    });
+  }
+
+  function halstead() {
+    stats.halstead = stats.count * Math.log2(stats.types);
   }
 
   function handlers() {
